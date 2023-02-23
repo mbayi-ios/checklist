9bd46a732b758d2f5525b94f7b3946691f46f426
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2023-02-23 14:40:21 +0300

    handling first time launch of the app

--- Checklist/DataModel.swift
+++ Checklist/DataModel.swift
@@ -13,6 +13,7 @@ class DataModel {
     init() {
         loadChecklists()
         registerDefaults()
+        handleFirstTime()
     }
 
     var indexOfSelectedChecklist: Int {
@@ -60,8 +61,24 @@ class DataModel {
     }
 
     func registerDefaults() {
-        let dictionary = [ "ChecklistIndex": -1]
+        let dictionary = [
+            "ChecklistIndex": -1,
+            "FirstTime": true
+        ] as [String: Any]
         UserDefaults.standard.register(defaults: dictionary)
+    }
+
+    func handleFirstTime() {
+        let userDefaults = UserDefaults.standard
+        let firstTime = userDefaults.bool(forKey: "FirstTime")
+
+        if firstTime {
+            let checklist = Checklist(name: "List")
+            lists.append(checklist)
+
+            indexOfSelectedChecklist = 0
+            userDefaults.set(false, forKey: "FirstTime")
+        }
     }
 
 }
