bcf6fa3092bc933a2ca448bf35683eeb8ce67fc7
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2023-02-24 22:33:11 +0300

    notification object on add a to-do item

--- Checklist/View Controllers/ItemDetailViewController.swift
+++ Checklist/View Controllers/ItemDetailViewController.swift
@@ -74,6 +74,17 @@ class ItemDetailViewController: UITableViewController {
         }
     }
 
+    @IBAction func shouldRemindToggled(_ switchControl: UISwitch) {
+        textField.resignFirstResponder()
+
+        if switchControl.isOn {
+            let center = UNUserNotificationCenter.current()
+            center.requestAuthorization(options: [.alert, .sound]) { _, _ in
+                // do nothing
+            }
+        }
+    }
+
     // MARK: - Table View Delegates
     override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
         return nil
